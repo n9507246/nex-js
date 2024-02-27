f178162415e0afbb1eb147efd68e5bbd9f1808af
Nikita Balashov <noWorkingEmail@mail.ru>
2024-02-27 12:16:24 +0500

    Next.js Blog Page Design

--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -11,6 +11,7 @@ const posts = [
 
   {title:'title', body: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt et possimus deleniti vel voluptatibus ea suscipit veritatis repudiandae, vitae a accusamus officiis accusantium dignissimos? Laboriosam omnis quae quia assumenda sapiente!', slug: 'asss-hole', img: 'https://i.pinimg.com/originals/d0/fb/de/d0fbde1f87000927d7d70f910965060c.jpg', createdAt:"01.01.2024"},
 ]
+
 const BlogPage = () => {
 
   return (
