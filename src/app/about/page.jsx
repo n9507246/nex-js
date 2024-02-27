21fc53254d8e296d1b173de2d94794e0c286f9ad
Nikita Balashov <noWorkingEmail@mail.ru>
2024-02-27 10:29:55 +0500

    About Page Design

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,5 +1,49 @@
-const AboutPage = ({}) => {
-  return <div>about</div>
-}
+import Image from "next/image";
+import classes from "./about.module.css";
 
-export default AboutPage
+
+
+
+const AboutPage = () => {
+
+  return (
+    <div className={classes.container}>
+      <div className={classes.textContainer}>
+        <h2 className={classes.subtitle}>About Agency</h2>
+        <h1 className={classes.title}>
+          We create digital ideas that are bigger, bolder, braver and better.
+        </h1>
+        <p className={classes.desc}>
+          We create digital ideas that are bigger, bolder, braver and better. We
+          believe in good ideas flexibility and precission We’re world’s Our
+          Special Team best consulting & finance solution provider. Wide range
+          of web and software development services.
+        </p>
+        <div className={classes.boxes}>
+          <div className={classes.box}>
+            <h1>10 K+</h1>
+            <p>Year of experience</p>
+          </div>
+          <div className={classes.box}>
+            <h1>10 K+</h1>
+            <p>Year of experience</p>
+          </div>
+          <div className={classes.box}>
+            <h1>10 K+</h1>
+            <p>Year of experience</p>
+          </div>
+        </div>
+      </div>
+      <div className={classes.imgContainer}>
+        <Image
+          src="/about.png"
+          alt="About Image"
+          fill
+          className={classes.img}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default AboutPage;
