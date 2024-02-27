17dc65fc55b9202fdf489c08db2884f765d20d8a
Nikita Balashov <noWorkingEmail@mail.ru>
2024-02-27 12:24:36 +0500

    Next.js Blog Page Design

--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -3,12 +3,8 @@ import classes from "./blog.module.css";
 
 const posts = [
   {title:'title', body: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt et possimus deleniti vel voluptatibus ea suscipit veritatis repudiandae, vitae a accusamus officiis accusantium dignissimos? Laboriosam omnis quae quia assumenda sapiente!', slug: 'asss-hole', img: 'https://i.pinimg.com/originals/d0/fb/de/d0fbde1f87000927d7d70f910965060c.jpg', createdAt:"01.01.2024"},
-
   {title:'title', body: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt et possimus deleniti vel voluptatibus ea suscipit veritatis repudiandae, vitae a accusamus officiis accusantium dignissimos? Laboriosam omnis quae quia assumenda sapiente!', slug: 'asss-hole', img: 'https://i.pinimg.com/originals/d0/fb/de/d0fbde1f87000927d7d70f910965060c.jpg', createdAt:"01.01.2024"},
-
   {title:'title', body: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt et possimus deleniti vel voluptatibus ea suscipit veritatis repudiandae, vitae a accusamus officiis accusantium dignissimos? Laboriosam omnis quae quia assumenda sapiente!', slug: 'asss-hole', img: 'https://i.pinimg.com/originals/d0/fb/de/d0fbde1f87000927d7d70f910965060c.jpg', createdAt:"01.01.2024"},
-
-
   {title:'title', body: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt et possimus deleniti vel voluptatibus ea suscipit veritatis repudiandae, vitae a accusamus officiis accusantium dignissimos? Laboriosam omnis quae quia assumenda sapiente!', slug: 'asss-hole', img: 'https://i.pinimg.com/originals/d0/fb/de/d0fbde1f87000927d7d70f910965060c.jpg', createdAt:"01.01.2024"},
 ]
 
